4f2830fa0428487300de71c93232033dc5d15393
hiroky1983 <hirockysan1983@gmail.com>
2025-01-26 05:35:35 +0900

    fix

--- array/reduce.go
+++ array/reduce.go
@@ -1,5 +1,7 @@
 package array
 
+// Reduce applies a reducer function to each element of the slice and returns the accumulated result.
+// If the input slice is nil, it returns the initial value.
 func Reduce[T any, U any](slice []T, initial U, reducer func(U, T) U) U {
 	if slice == nil {
 		return initial
