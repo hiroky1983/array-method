2093307b84c954261658acedebf768e85dc3944d
hiroky1983 <hirockysan1983@gmail.com>
2025-01-26 14:26:39 +0900

    reduceの実装とインスタンスの型調整

--- array/reduce.go
+++ array/reduce.go
@@ -2,7 +2,7 @@ package array
 
 // Reduce applies a reducer function to each element of the slice and returns the accumulated result.
 // If the input slice is nil, it returns the initial value.
-func Reduce[T any, U any](slice []T, initial U, reducer func(U, T) U) U {
+func Reduce[T any, U any](slice []T, reducer func(U, T) U, initial U) U {
 	if slice == nil {
 		return initial
 	}
